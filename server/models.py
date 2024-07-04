cf0ccc29095a8a62599426d3d896b9495c30d461
Felix Mutuma <felicmutuma8@gmail.com>
2024-07-04 21:14:28 +0300

    commits flask validations lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,16 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Name cannot be null')
+        return name
+    @validates('phone_number')
+    def validate_phone_no(self,key,phone_number):
+        if phone_number and not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError('Phone number must be exactly 10 digits!')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -29,6 +39,30 @@ class Post(db.Model):
 
     # Add validators  
 
+    @validates('title')
+    def validate_title(self,key,title):
+        click_baits = ["Won't beleive","Secret","Top","Guess"]
+        if not any(keyword in title for keyword in click_baits):
+            raise ValueError('Title must contain a clickbait!')
+        return title
+
+    @validates('content')
+    def validate_content(self,key,content):
+        if len(content.strip()) < 250:
+            raise ValueError('Content must be 250 words and above!')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary.strip()) > 250:
+            raise ValueError("Summary must be at most 250 characters long.")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
